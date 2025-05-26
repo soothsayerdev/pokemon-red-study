6ff1ada7081e5211c040bfa1241a6ff64d6593ff
Diogo L Andrade <dlourenco521@gmail.com>
2025-05-26 16:30:42 -0300

    base cerulean code

--- internal/gym/cerulean.go
+++ internal/gym/cerulean.go
@@ -1,2 +1,17 @@
 package gym
-// implementar gym misty 
+
+// implementar gym misty
+
+type Cerulean struct {
+	trainer trainer
+	name    string
+	Type    string
+}
+
+func NewCeruleanGym(name string, trainer trainer) *Cerulean {
+	return &Cerulean{
+		name:    name,
+		Type:    "Water",
+		trainer: trainer,
+	}
+}
